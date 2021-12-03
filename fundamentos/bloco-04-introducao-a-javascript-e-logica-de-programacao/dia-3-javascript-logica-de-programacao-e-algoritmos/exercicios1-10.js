4fe2bec38804a80e2a417dfe4f41b70f0d5f0fa8
Francine <francinexfj@gmail.com>
2021-12-03 15:56:28 +0100

    Exercicio 2 - Inverter palavra

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios1-10.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios1-10.js
@@ -12,7 +12,10 @@ console.log(fatorial);
 // 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 // let word = 'tryber';
 
+let word = 'tryber';
+let wordInverse = word.split('').reverse().join('');
 
+console.log(wordInverse);
 
 // 3- Considere o array de strings abaixo:
 // Copiar
