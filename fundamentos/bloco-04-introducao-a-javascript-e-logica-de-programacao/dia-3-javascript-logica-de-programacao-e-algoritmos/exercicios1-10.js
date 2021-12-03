edbb2ccbbbcdba11a76c35c44a8e3992a1896e4c
Francine <francinexfj@gmail.com>
2021-12-03 22:48:40 +0100

    Exercicio 4 - Maior número primo entre 0 e 50

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios1-10.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios1-10.js
@@ -43,3 +43,22 @@ console.log("Menor string: " + menor);
 
 
 // 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+
+let maiorNumeroPrimo = 0;
+
+for (let numeroAtual = 0; numeroAtual <= 50; numeroAtual += 1) {
+  let isPrimo = true;
+
+  for (let divisorAtual = 2; divisorAtual < numeroAtual; divisorAtual += 1) {
+
+    if (numeroAtual % divisorAtual === 0) {
+      isPrimo = false;
+    }
+  }
+  
+  if (isPrimo) {
+    maiorNumeroPrimo = numeroAtual;
+  }
+}
+
+console.log(maiorNumeroPrimo);
