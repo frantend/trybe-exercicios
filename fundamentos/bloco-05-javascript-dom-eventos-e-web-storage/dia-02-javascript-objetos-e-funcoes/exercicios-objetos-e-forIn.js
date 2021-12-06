5b65cc8c726f989eb57e877bd914e3ffbd82d5d3
Francine <francinexfj@gmail.com>
2021-12-06 15:17:26 +0100

    Exercicio 6 - Objetos e For/In

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-objetos-e-funcoes/exercicios-objetos-e-forIn.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-objetos-e-funcoes/exercicios-objetos-e-forIn.js
@@ -62,3 +62,24 @@ for (let properties in info) {
     console.log(info[properties] + ' e ' + info2[properties]);
   }
 }
+
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
+
