fb4ffb1d5020139f45672af8bc019d3767c6e858
Francine <francinexfj@gmail.com>
2021-12-06 15:17:43 +0100

    Exercicio 7 - Objetos e For/In

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-objetos-e-funcoes/exercicios-objetos-e-forIn.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-objetos-e-funcoes/exercicios-objetos-e-forIn.js
@@ -83,3 +83,13 @@ let leitor = {
 
 console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '"');
 
+// 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+
+leitor.livrosFavoritos.push(
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  },
+);
+
