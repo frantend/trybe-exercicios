5ff2b9044584b98af63f22f97adb51b227580cbe
Francine <francinexfj@gmail.com>
2022-03-03 20:17:11 +0100

    Exercicios de fixacao 2 e 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1.js
@@ -11,6 +11,10 @@
 
 // testingScope(true);
 
+// 1. Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// 2. Modifique a estrutura da função para que ela seja uma arrow function .
+// 3. Modifique as concatenações para template literals.
+
 const testingScope = escopo => {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
