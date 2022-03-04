98a93818963851c9408291e3268c02261ecef45e
Francine <francinexfj@gmail.com>
2022-03-04 17:21:20 +0100

    Exercicios de fixacao parte 2 + Bonus

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1.js
@@ -28,3 +28,39 @@ const testingScope = escopo => {
 
 testingScope(true);
 
+
+// Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o método array.sort() . Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// const sortOddsAndEvents = () => {
+//   oddsAndEvens[0] = 2;
+//   oddsAndEvens[1] = 3;
+//   oddsAndEvens[2] = 4;
+//   oddsAndEvens[3] = 7;
+//   oddsAndEvens[4] = 10;
+//   oddsAndEvens[5] = 13;
+
+//   return oddsAndEvens
+
+// }
+
+// const sorteArray = sortOddsAndEvents()
+
+// console.log(`Os números ${sorteArray} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
+
+
+// Bônus array.sort()
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortArrayBonus = array => {
+  const sortOddsAndEvens = array.sort((a, b) => a - b);
+  return sortOddsAndEvens;
+}
+
+const sortedArrayBonus = sortArrayBonus(oddsAndEvens);
+console.log(`Os números ${sortedArrayBonus} se encontram ordenados de forma crescente !`);
+
