8306d0768652089eccfa190a913325c4bccc146c
Francine <francinexfj@gmail.com>
2022-08-21 17:14:51 +0200

    exercicios 1 a 4

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,77 @@
+// 15 Exercícios de Lógica, utilizando: lógica condicional (if/else e switch/case), operadores aritméticos (+, -, *, /, %) e operadores lógicos (>, <, &&, ||).
+
+/* 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+const a = 2;
+const b = 3;
+let operacao = 'Módulo';
+
+switch(operacao) {
+
+  case "Adição":
+    console.log(a + b);
+    break;
+
+  case "Subtração":
+    console.log(a - b);
+    break;
+
+  case "Multiplicação":
+    console.log(a * b);
+    break;
+
+  case "Divisão":
+    console.log(a / b);
+    break;
+
+  case "Módulo":
+    console.log(a % b);
+    break;
+
+  default:
+    console.log('Operação indisponível');
+}
+
+
+/* 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. */
+
+const n1 = 10;
+const n2 = 50;
+
+if(n1 > n2) {
+  console.log(`${n1} é o maior.`);
+} else {
+  console.log(`${n2} é o maior`);
+}
+
+
+/* 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados. */
+
+const n1 = 100;
+const n2 = 150;
+const n3 = 75;
+
+if(n1 > n2 && n1 > n3) {
+  console.log(`${n1} é o maior`);
+} else if(n2 > n1 && n2 > n3) {
+  console.log(`${n2} é o maior`);
+} else {
+  console.log(`${n3} é o maior`);
+}
+
+/* 4 - Faça um programa que, dado um valor recebido como parâmetro, retorne "positive" se esse valor for positivo, "negative" se for negativo, e caso não seja nem positivo e nem negativo retorne "zero". */
+
+const valor = 1;
+
+if(valor > 0) {
+  console.log('positive');
+} else if(valor < 0) {
+  console.log('negative');
+} else if(valor === 0) {
+  console.log('zero')
+}
