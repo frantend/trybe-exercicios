4969a57e96f56f70bb82045c08a83bebb81ed746
Francine <francinexfj@gmail.com>
2022-09-01 00:10:06 +0200

    Exercicio 2 - Implementar uma função que crie dinamicamente um botão com o nome "Feriados"

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-eventos/script.js
@@ -13,7 +13,7 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Exercício 1:
+// Exercício 1: Criar um calendário dinamicamente.
 
 let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
@@ -50,3 +50,19 @@ function criarDiasDoMes() {
 
 criarDiasDoMes();
 
+
+// Exercicio 2 - Implementar uma função que crie dinamicamente um botão com o nome "Feriados".
+
+function botaoFeriados(nome) {
+  const botaoContainer = document.querySelector('.buttons-container');
+
+  const botaoFeriados = document.createElement('button');
+
+  botaoFeriados.id = 'btn-holiday';
+
+  botaoFeriados.innerHTML = nome;
+
+  botaoContainer.appendChild(botaoFeriados);
+}
+
+botaoFeriados('Feriados');
