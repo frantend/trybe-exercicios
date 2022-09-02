166c7bd9b558ef933e71a69a4bb048587d42794a
Francine <francinexfj@gmail.com>
2022-09-02 13:09:16 +0200

    Exercício 3: // Implemente uma função que muda a cor de fundo dos dias que possuem a classe "holiday

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-03-eventos/script.js
@@ -66,3 +66,26 @@ function botaoFeriados(nome) {
 }
 
 botaoFeriados('Feriados');
+
+
+// Exercício 3:
+// Implemente uma função que muda a cor de fundo dos dias que possuem a classe "holiday".
+
+function displayHolidays() {
+  let getHolidayButton = document.querySelector('#btn-holiday');
+  let getHolidays = document.querySelectorAll('.holiday')
+  let backgroundColor = 'rgb(238,238,238)';
+  let setNewColor = 'white';
+
+  getHolidayButton.addEventListener('click', function() {
+    for (let index = 0; index < getHolidays.length; index += 1) {
+      if (getHolidays[index].style.backgroundColor === setNewColor) {
+        getHolidays[index].style.backgroundColor = backgroundColor;
+      } else {
+        getHolidays[index].style.backgroundColor = setNewColor;
+      }
+    }
+  });
+}
+
+displayHolidays();
