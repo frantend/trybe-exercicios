62be6cae0f8d0f2ed17ebd3e396bd88647dfb343
Fran <fran@MacBook-Pro-de-JF.local>
2023-02-04 16:30:15 +0100

    Salvar estilos no localStorage

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-04-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-04-web-storage/script.js
@@ -3,6 +3,7 @@ const titulo = document.querySelector('.titulo');
 const subtitulo = document.querySelector('.subtitulo');
 const texto = document.querySelector('.texto');
 
+
 // Mudar cor do funto
 const btnBackgroundWhite = document.querySelector('.btn-background-white');
 const btnBackgroundBlack = document.querySelector('.btn-background-black');
@@ -12,22 +13,27 @@ const btnBackgroundYellow = document.querySelector('.btn-background-yellow');
 
 btnBackgroundWhite.addEventListener('click', () => {
   body.style.backgroundColor = 'white';
+  localStorage.setItem('background-color', body.style.backgroundColor = 'white');
 });
 
 btnBackgroundBlack.addEventListener('click', () => {
   body.style.backgroundColor = 'black';
+  localStorage.setItem('background-color', body.style.backgroundColor = 'black');
 });
 
 btnBackgroundGreen.addEventListener('click', () => {
   body.style.backgroundColor = 'green';
+  localStorage.setItem('background-color', body.style.backgroundColor = 'green');
 });
 
 btnBackgroundBlue.addEventListener('click', () => {
   body.style.backgroundColor = 'blue';
+  localStorage.setItem('background-color', body.style.backgroundColor = 'blue');
 });
 
 btnBackgroundYellow.addEventListener('click', () => {
   body.style.backgroundColor = 'yellow';
+  localStorage.setItem('background-color', body.style.backgroundColor = 'yellow');
 });
 
 
@@ -40,23 +46,37 @@ btnColorBlack.addEventListener('click', () => {
   titulo.style.color = 'black';
   subtitulo.style.color = 'black';
   texto.style.color = 'black';
+  localStorage.setItem('color', 
+    titulo.style.color = 'black',
+    subtitulo.style.color = 'black',
+    texto.style.color = 'black'
+    )
 });
 
 btnColorRed.addEventListener('click', () => {
   titulo.style.color = 'red';
   subtitulo.style.color = 'red';
   texto.style.color = 'red';
+  localStorage.setItem('color', 
+    titulo.style.color = 'red',
+    subtitulo.style.color = 'red',
+    texto.style.color = 'red'
+    )
 });
 
 btnColorWhite.addEventListener('click', () => {
   titulo.style.color = 'white';
   subtitulo.style.color = 'white';
   texto.style.color = 'white';
+  localStorage.setItem('color', 
+    titulo.style.color = 'white',
+    subtitulo.style.color = 'white',
+    texto.style.color = 'white'
+    )
 });
 
 
 // Mudar tamanho da fonte
-
 const btnSize8 = document.querySelector('.btn-size-8');
 const btnSize10 = document.querySelector('.btn-size-10');
 const btnSize12 = document.querySelector('.btn-size-12');
@@ -67,33 +87,48 @@ btnSize8.addEventListener('click', () => {
   titulo.style.fontSize = '8px';
   subtitulo.style.fontSize = '8px';
   texto.style.fontSize = '8px';
+  localStorage.setItem('tamanho-fonte', titulo.style.fontSize = '8px',
+  subtitulo.style.fontSize = '8px',
+  texto.style.fontSize = '8px',);
 });
 
 btnSize10.addEventListener('click', () => {
   titulo.style.fontSize = '10px';
   subtitulo.style.fontSize = '10px';
   texto.style.fontSize = '10px';
+  localStorage.setItem('tamanho-fonte', titulo.style.fontSize = '10px',
+  subtitulo.style.fontSize = '10px',
+  texto.style.fontSize = '10px',);
 });
 
 btnSize12.addEventListener('click', () => {
   titulo.style.fontSize = '12px';
   subtitulo.style.fontSize = '12px';
   texto.style.fontSize = '12px';
+  localStorage.setItem('tamanho-fonte', titulo.style.fontSize = '12px',
+  subtitulo.style.fontSize = '12px',
+  texto.style.fontSize = '12px',);
 });
 
 btnSize14.addEventListener('click', () => {
   titulo.style.fontSize = '14px';
   subtitulo.style.fontSize = '14px';
   texto.style.fontSize = '14px';
+  localStorage.setItem('tamanho-fonte', titulo.style.fontSize = '14px',
+  subtitulo.style.fontSize = '14px',
+  texto.style.fontSize = '14px',);
 });
 btnSize20.addEventListener('click', () => {
   titulo.style.fontSize = '20px';
   subtitulo.style.fontSize = '20px';
   texto.style.fontSize = '20px';
+  localStorage.setItem('tamanho-fonte', titulo.style.fontSize = '20px',
+  subtitulo.style.fontSize = '20px',
+  texto.style.fontSize = '20px',);
 });
 
-// Mudar espaçamento entre as linhas
 
+// Mudar espaçamento entre as linhas
 const btnLine1 = document.querySelector('.btn-line-1');
 const btnLineNormal = document.querySelector('.btn-line-normal');
 const btnLine15 = document.querySelector('.btn-line-1-5');
@@ -102,26 +137,31 @@ const btnLine3 = document.querySelector('.btn-line-3');
 
 btnLine1.addEventListener('click', () => {
   texto.style.lineHeight = '1';
+  localStorage.setItem('altura-linha', texto.style.lineHeight = '1');
 });
 
 btnLineNormal.addEventListener('click', () => {
   texto.style.lineHeight = 'normal';
+  localStorage.setItem('altura-linha', texto.style.lineHeight = 'normal');
 });
 
 btnLine15.addEventListener('click', () => {
   texto.style.lineHeight = '1.5';
+  localStorage.setItem('altura-linha', texto.style.lineHeight = '1.5');
 });
 
 btnLine2.addEventListener('click', () => {
   texto.style.lineHeight = '2';
+  localStorage.setItem('altura-linha', texto.style.lineHeight = '2');
 });
 
 btnLine3.addEventListener('click', () => {
   texto.style.lineHeight = '3';
+  localStorage.setItem('altura-linha', texto.style.lineHeight = '2');
 });
 
-// Mudar tipo da fonte
 
+// Mudar tipo da fonte
 const btnFontArial = document.querySelector('.btn-font-arial');
 const btnFontTimes = document.querySelector('.btn-font-times');
 
@@ -129,30 +169,45 @@ btnFontArial.addEventListener('click', () => {
   titulo.style.fontFamily = 'Arial';
   subtitulo.style.fontFamily = 'Arial';
   texto.style.fontFamily = 'Arial';
+  localStorage.setItem('fonte-family', titulo.style.fontFamily = 'Arial',
+  subtitulo.style.fontFamily = 'Arial',
+  texto.style.fontFamily = 'Arial');
 });
 
 btnFontTimes.addEventListener('click', () => {
   titulo.style.fontFamily = 'Times';
   subtitulo.style.fontFamily = 'Times';
   texto.style.fontFamily = 'Times';
+  localStorage.setItem('fonte-family', titulo.style.fontFamily = 'Times',
+  subtitulo.style.fontFamily = 'Times',
+  texto.style.fontFamily = 'Times');
 });
 
-// Salva os estilos
 
-const salvaEstilos = () => {
-  const estilos = {
-    body: '',
-    titulo: '',
-    subtitulo: '',
-    texto: '',
-  };
-  
-  estilos.body = document.querySelector('.body').style.backgroundColor;
-  estilos.titulo = document.querySelector('.titulo').style;
-  estilos.subtitulo = document.querySelector('.subtitulo').style;
-  estilos.texto = document.querySelector('.texto').style;
-  
-  localStorage.setItem(JSON.stringify(estilos))
-  
-};
 
+
+
+// Recupera estilos
+window.onload = function() {
+  let salvaBackground = localStorage.getItem('background-color');
+  body.style.backgroundColor = salvaBackground;
+
+  let color = localStorage.getItem('color');
+  titulo.style.color = color;
+  subtitulo.style.color = color;
+  texto.style.color = color;
+
+  let tamanhoFonte = localStorage.getItem('tamanho-fonte');
+  titulo.style.fontSize = tamanhoFonte;
+  subtitulo.style.fontSize = tamanhoFonte;
+  texto.style.fontSize = tamanhoFonte;
+
+  let alturaLinha = localStorage.getItem('altura-linha');
+  texto.style.lineHeight = alturaLinha;
+
+  let fonteFamily = localStorage.getItem('fonte-family');
+  titulo.style.fontFamily = fonteFamily;
+  subtitulo.style.fontFamily = fonteFamily;
+  texto.style.fontFamily = fonteFamily;
+
+}
