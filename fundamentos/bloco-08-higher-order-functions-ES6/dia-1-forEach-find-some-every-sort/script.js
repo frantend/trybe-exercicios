80e855d35145f60be58e5a53af1df6f6699e904b
frantend <francinexfj@gmail.com>
2023-07-14 20:50:09 +0200

    Atualizar pastas

--- fundamentos/bloco-08-higher-order-functions-ES6/dia-1-forEach-find-some-every-sort/script.js
+++ fundamentos/bloco-08-higher-order-functions-ES6/dia-1-forEach-find-some-every-sort/script.js
@@ -26,7 +26,7 @@ console.log(newEmployees());
 // Desenvolva uma HOF que retorne o resultado de um sorteio. Essa HOF irá gerar um número aleatório entre 1 e 5, recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns, você ganhou”).
 
 const sorteadorLoteria = (numero) => {
-  const numeroAleatorio = Math.floor(Math.random() * (5 - 1 + 1));
+  const numeroAleatorio = Math.floor(Math.random() * 5) + 1;
 
   console.log(numero === numeroAleatorio ? `Número sorteado: ${numeroAleatorio} - Parabéns, você ganhou` : `Número sorteado: ${numeroAleatorio} - Tente novamente`)
   
