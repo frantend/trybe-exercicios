1a157c00998b300a5b79cca35c9d497048ee3025
frantend <francinexfj@gmail.com>
2023-07-25 21:46:22 +0200

    exercicio com api superheroes: tratar erro com sweetalert

--- frontend/js-assincrono-promises/app-superheroe/main.js
+++ frontend/js-assincrono-promises/app-superheroe/main.js
@@ -1,4 +1,5 @@
 import './style.css';
+import Swal from 'sweetalert2';
 
 const imagem = document.getElementById('imagem');
 const nome = document.getElementById('nome');
@@ -7,14 +8,19 @@ const botao = document.getElementById('botao');
 const BASE_URL_TOKEN = 'https://superheroapi.com/api/6156720867705062/';
 
 
-botao.addEventListener('click', () => {
-  const id = Math.floor(Math.random() * 100);
+botao.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const id = Math.floor(Math.random() * 1000);
+
   fetch(`https://superheroapi.com/api.php/6156720867705062/${id}`)
   .then(response => response.json())
   .then(data => {
+    console.log(data.id);
     imagem.src = `${data.image.url}`
     imagem.alt = `${data.name} id:${data.id}`
     nome.innerHTML = `${data.name}`
   })
+  .catch(() => Swal.fire('Heroe não encontrado, tente novamente!'));
 });
 
